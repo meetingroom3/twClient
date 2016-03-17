cc5ee004dc3d325cdbc8b8a2816ce120e2aa7b1d
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2016-03-17 19:40:53 +0900

    first commit

---
+++ client/app/main/main.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('twClientApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+  });
