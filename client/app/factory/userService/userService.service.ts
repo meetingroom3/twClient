9be4b2dde333fa721a2b2c06b9bfa7f6aa7335f5
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2016-03-28 18:45:23 +0900

    #12 directiveは一旦スキップ

--- client/app/factory/userService/userService.service.ts
+++ client/app/factory/userService/userService.service.ts
@@ -5,19 +5,13 @@ namespace twClientApp {
   'use strict';
 
   export class UserController {
-    static $inject = ['appConfig'];
-    constructor(public APP_CONF) {
-    }
-
-    public getUserServiceId():string {
-      return this.APP_CONF.userServiceId;
+    constructor() {
     }
   }
 
   angular.module('twClientApp')
     .factory('userService', [
-      'appConfig',
-      (APP_CONF)=>{ return new UserController(APP_CONF) }] );
+      ()=>{ return new UserController() }] );
 }
 
 
